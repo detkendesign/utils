b63a6d3733ad5738e44a43d9aba16437343de426
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2025-10-12 22:43:18 +0200

    Add constants for time handling.

---
+++ src/lib/times/constants.ts
@@ -0,0 +1,16 @@
+export const SECONDS_IN_MINUTE = 60;
+export const MINUTES_IN_HOUR = 60;
+export const HOURS_IN_DAY = 24;
+export const DAYS_IN_WEEK = 7;
+export const DAYS_IN_MONTH = 30;
+export const DAYS_IN_YEAR = 365;
+
+export const SECONDS_IN_HOUR = MINUTES_IN_HOUR * SECONDS_IN_MINUTE;
+export const SECONDS_IN_DAY = HOURS_IN_DAY * SECONDS_IN_HOUR;
+
+export const MILLIS_IN_SECOND = 1000;
+export const MILLIS_IN_MINUTE = MILLIS_IN_SECOND * SECONDS_IN_MINUTE;
+export const MILLIS_IN_HOUR = MILLIS_IN_SECOND * SECONDS_IN_HOUR;
+export const MILLIS_IN_DAY = MILLIS_IN_SECOND * SECONDS_IN_DAY;
+
+export const MONTHS_IN_A_YEAR = 12;
