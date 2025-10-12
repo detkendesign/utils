2ccb6ddc81c2a341d9cc63bbc2cd69ac857c0084
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2025-10-12 22:36:47 +0200

    Add TOmit type, a typesafe Omit wrapper.

--- src/lib/general/types.ts
+++ src/lib/general/types.ts
@@ -18,3 +18,12 @@ export type RequireKeys<T, K extends keyof T> = T & { [P in K]-?: T[P] };
  * userIdToUserMap["123"] = { name: "John Doe" };
  */
 export type StringToTypeMap<T> = { [key in string]: T };
+
+/**
+ * Typesafe Omit.
+ *
+ * @example
+ * type Test = TOmit<{ a: number; b: string }, "a">;
+ *        ^ { b: string }
+ */
+export type TOmit<T, K extends keyof T> = Omit<T, K>;
