c8b68573c2e92cc4d3c654f86689e1eef833f778
Anton Detken <69902586+ant0n7@users.noreply.github.com>
2025-10-12 22:52:14 +0200

    Add utility function for unreachable unions.

---
+++ src/lib/general/utils.ts
@@ -0,0 +1,27 @@
+/**
+ * Creates the unreachable union error, supporting the unreachable function.
+ */
+export const createUnreachableError = (action: string, message?: string) => {
+  return new Error(message ?? `Unreachable action received for ${action}`);
+};
+
+/**
+ * Throws an error when an unreachable action is received.
+ *
+ * Perfect for function endings that are guaranteed to never be reached, to be
+ * able to return the full type, instead of undefined.
+ *
+ * @example Keeping a function return type complete.
+ * ```ts
+ * const foo = (): string => {
+ *   // If statements.
+ *   return unreachable("foo");
+ * };
+ * ```
+ *
+ * @param action - The action that was received, e.g. a function name.
+ * @param message - The message to throw.
+ */
+export const unreachable = (action: string, message?: string) => {
+  throw createUnreachableError(action, message);
+};
